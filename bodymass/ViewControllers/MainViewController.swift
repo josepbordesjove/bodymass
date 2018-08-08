bec8b691777220f540e77a3c5457c366e0246291
Josep Bordes <josepbordesjove@gmail.com>
2018-08-08 21:28:49 +0200

    Prepares the project to start working on it

---
+++ bodymass/ViewControllers/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  bodymass
+//
+//  Created by Josep Bordes Jové on 5/8/18.
+//  Copyright © 2018 Josep Bordes Jové. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .blue
+  }
+}
+
