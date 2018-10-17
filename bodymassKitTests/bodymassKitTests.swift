597c2b31b63c7728c61c45faaa08ae73860d5ace
Josep Bordes <josepbordesjove@gmail.com>
2018-10-18 00:23:22 +0200

    Prepare for start testing

--- bodymassKitTests/bodymassKitTests.swift
+++ bodymassKitTests/bodymassKitTests.swift
@@ -7,27 +7,27 @@
 //
 
 import XCTest
+import bodymassKit
 
-class BodymassKitTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class bodymassKitTests: XCTestCase {
+  
+  struct Constants {
+    static let height: Double = 189
+    static let weight: Double = 79
+  }
+  
+  var dataStore: DataStore!
+  
+  override func setUp() {
+    dataStore = DataStore()
+  }
+  
+  func testSaveDataPoint() {
+    dataStore.loadAndMigrateIfNeeded {
+      self.dataStore.saveDataPoint(id: UUID().uuidString, weight: Constants.weight, height: Constants.height, completion: {
+       XCTAssert(true)
+      })
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+  }
+  
 }
